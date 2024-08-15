3e77894dbd1624a01fd04709f4d8b22e72767f45
Sriram <78687650+SriramRayala@users.noreply.github.com>
2024-08-15 18:31:40 -0400

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,7 +13,7 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/Shalem2020",
+  github: "https://github.com/SriramRayala",
   resume:"https://drive.google.com/file/d/12e1cjf9O0wJJuOTzg69E1oBhzqM_PCvd/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shalemrajumaddirala/",
   twitter: "https://twitter.com/ShalemRaju8247",
