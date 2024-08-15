5f670b7d33a8636c50852a55cd10653e2ade2f38
Sriram <78687650+SriramRayala@users.noreply.github.com>
2024-08-15 18:37:17 -0400

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,6 @@ export const Bio = {
     "Cybersecurity Analyst",
     "Ethical Hacker",
     "Pen Tester",
-    "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
