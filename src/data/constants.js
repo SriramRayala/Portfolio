e13eb255c9f59765f622aab8f554969fd412dcf5
Sriram <78687650+SriramRayala@users.noreply.github.com>
2024-08-15 18:27:32 -0400

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,7 +4,7 @@ import bankImage from "../images/demobank.png";
 import passwordImage from "../images/password.png";
 
 export const Bio = {
-  name: "Sriram Rayala",
+  name: "Sriram Rayala ",
   roles: [
     "Cybersecurity Analyst",
     "Ethical Hacker",
