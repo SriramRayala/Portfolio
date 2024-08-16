8c79d3539a3347fc8a1438fb3007ea34b3364945
Sriram <78687650+SriramRayala@users.noreply.github.com>
2024-08-16 16:34:22 -0400

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/Ethical Image.jpg'
+import HeroImg from '../../images/IMG1.jpeg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
