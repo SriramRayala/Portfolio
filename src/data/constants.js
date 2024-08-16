611bdbc8f5529e4ae302fd141729da3d505def9f
Sriram <78687650+SriramRayala@users.noreply.github.com>
2024-08-16 16:42:09 -0400

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,12 +4,11 @@ import bankImage from "../images/demobank.png";
 import passwordImage from "../images/password.png";
 
 export const Bio = {
-  name: "Shalem Raju Maddirala",
-  roles: [
-    "Cybersecurity Analyst",
-    "Ethical Hacker",
-    "Pen Tester",
-    "Programmer",
+  name: "Sriram Rayal[
+    "Cybersecurity Student",
+    "Blue Teamer",
+    "Web PenTester",
+    "OSINT Analyst",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
